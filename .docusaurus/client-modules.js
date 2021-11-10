171eac8e67f253decdc933a28f492ae62156e97a
vwumumu <vwumumu@gmail.com>
2021-11-10 14:55:39 +0800

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/vwumumu/github/mixin123/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/vwumumu/github/mixin123/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/vwumumu/github/mixin123/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/vwumumu/github/mixin123/src/css/custom.css"),
+];
