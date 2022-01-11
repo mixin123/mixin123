e79a25a9ab40a1d9822613d66c1b567fbb7db38c
vwumumu <vwumumu@gmail.com>
2022-01-11 15:25:35 +0800

    remove docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/vwumumu/github/mixin123/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/vwumumu/github/mixin123/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/vwumumu/github/mixin123/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/vwumumu/github/mixin123/src/css/custom.css"),
-];
